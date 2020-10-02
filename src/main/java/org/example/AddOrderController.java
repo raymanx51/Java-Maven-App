b8abf6499e2dbe687e9afb18715b2c632855eb99
CalShark21 <callumshark@yahoo.com>
2020-10-02 15:55:37 -0400

    Implemented "add order" window
    Also note: Updated libraries to include JavaFX (may cause errors)

--- src/main/java/org/example/AddOrderController.java
+++ src/main/java/org/example/AddOrderController.java
@@ -1,10 +1,48 @@
 package org.example;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddOrderController {
 
+    public AnchorPane layout;
+    @FXML private TextField lastNameTextField;
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField invoiceNumTextField;
+
+    // Establishes a reference to the controller that created this window
     public PrimaryController parentController;
 
     public void setParentController(PrimaryController controller) {
         parentController = controller;
     }
+
+    @FXML
+    private void addOrder() throws IOException {
+        // Singleton testing
+        Entry ent = new Entry();
+
+        ent.setLastName(lastNameTextField.getText());
+        ent.setFirstName(firstNameTextField.getText());
+        ent.setOrderNum(invoiceNumTextField.getText());
+        SingletonData holder = SingletonData.getInstance();
+        holder.setEntry(ent);
+
+        parentController.parseNewOrder();
+
+        closeWindow();
+    }
+
+
+    @FXML
+    private void closeWindow () throws IOException {
+        Stage window = (Stage) ((Node)layout).getScene().getWindow();
+        window.close();
+    }
+
 }
--- src/main/java/org/example/Entry.java
+++ src/main/java/org/example/Entry.java
@@ -3,16 +3,35 @@ package org.example;
 public class Entry {
 
     public String name;
+
+    public String firstName;
+    public String lastName;
     public String orderNum;
 
 
-    public void setName(String n){
 
+    public void setName(String n){
         name = n;
     }
 
     public String getName(){
         return name;
+    }
+
+    public void setFirstName(String fName){
+        firstName = fName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lName) {
+        lastName = lName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public void setOrderNum(String o){
