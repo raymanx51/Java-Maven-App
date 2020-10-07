58db343c4e5f22554ee2cbc6c58f92c3d72977f3
CalShark21 <callumshark@yahoo.com>
2020-10-07 10:30:44 -0400

    Added "orders" page, tweaked FX layout

---
+++ src/main/java/org/example/AddCustomerController.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class AddCustomerController {
+
+    // Establishes a reference to the controller that created this window
+    public DashboardController parentController;
+
+    public void setParentController(DashboardController controller) {
+        parentController = controller;
+    }
+}
